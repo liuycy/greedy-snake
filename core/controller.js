fd246de9fe814c886fbe0c2e67a714917fcaeb93
liuycy <liuycy@yeah.net>
2020-11-22 14:39:46 +0800

    refactor: new keypress event

--- core/controller.js
+++ core/controller.js
@@ -1,17 +1,28 @@
 const readline = require('readline')
-const MuteStream = require('mute-stream')
 
-const output = new MuteStream()
-output.pipe(process.stdout)
-output.mute()
+// 方案一
+// const MuteStream = require('mute-stream')
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output,
-})
+// const output = new MuteStream()
+// output.pipe(process.stdout)
+// output.mute()
 
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output,
+// })
+
+// exports.on = function (handler) {
+//   rl.input.on('keypress', (_, event) => {
+//     handler(event.name)
+//   })
+// }
+
+// 方案二
+readline.emitKeypressEvents(process.stdin)
+process.stdin.setRawMode(true)
 exports.on = function (handler) {
-  rl.input.on('keypress', (_, event) => {
+  process.stdin.on('keypress', (_, event) => {
     handler(event.name)
   })
 }
